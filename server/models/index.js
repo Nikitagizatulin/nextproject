5b9cc854b7b6464e29155407f2eca3bb7ff09359
nikita <nikitagizatulin@gmail.com>
2019-06-17 17:13:49 +0300

    updated funciton of  mongodb connection

--- server/models/index.js
+++ server/models/index.js
@@ -4,24 +4,32 @@ const mongoose = require('mongoose');
 const database = process.env.DB_NAME || 'todo';
 const authSource = process.env.DB_AUTH_SOURCE;
 let uri = `mongodb://localhost/${database}`;
+let options = {
+    useCreateIndex: true,
+    useNewUrlParser: true
+};
 
 if (authSource) {
-  uri += `?authSource=${authSource}`;
+    uri += `?authSource=${authSource}`;
+}
+if (process.env.DB_USER && process.env.DB_PASS) {
+    options.user = process.env.DB_USER;
+    options.pass = process.env.DB_PASS;
 }
 
-mongoose.connect(uri, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-});
+mongoose.connect(uri, options);
 const dbs = {
-  mongoose,
+    mongoose
 };
 
 fs.readdirSync(__dirname)
-  .filter(file => file !== 'index.js')
-  .forEach((file) => {
-    const modelName = file.replace('.js', '');
-    dbs[modelName] = mongoose.model(modelName, require(`./${file}`)(mongoose));
-  });
+    .filter(file => file !== 'index.js')
+    .forEach(file => {
+        const modelName = file.replace('.js', '');
+        dbs[modelName] = mongoose.model(
+            modelName,
+            require(`./${file}`)(mongoose)
+        );
+    });
 
 module.exports = dbs;
