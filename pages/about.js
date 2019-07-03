9579d84eaceff350651c1cd9a5fd46d3c87d6085
nikita <nikitagizatulin@gmail.com>
2019-07-03 17:41:09 +0300

    updated about page

--- pages/about.js
+++ pages/about.js
@@ -18,7 +18,7 @@ export default class About extends React.Component {
                     <Col span={18} offset={3}>
                         <Title level={3} className="about-page_subtitle">
                             It&apos;s simple way to stored your tasks. You can
-                            edit your task, delete and set to completed status.
+                            add your task, delete and set to completed status.
                             For started to use the application, please log in or
                             sign up.
                         </Title>
@@ -28,7 +28,7 @@ export default class About extends React.Component {
                         <ul className="technologies-list">
                             <li>
                                 Server: Node.js / Express.js / Mongoose /
-                                Next.js
+                                Next.js / Jsonwebtoken / Passport.js
                             </li>
                             <li>
                                 Front-end: Ant design / Next.js / React / Redux
@@ -41,8 +41,8 @@ export default class About extends React.Component {
                     <Col span={18} offset={3}>
                         <p className="paragraph">
                             Also in this application, I use user authentication
-                            through cookies, and not through locale as in a
-                            normal spa application
+                            through cookies, and not through local storage as in
+                            a normal spa application
                         </p>
                         <p className="paragraph">
                             This allows us to retrieve and verify the
